8312c39612a1e9d449a18f36314315b3e118f936
Richard Wright <richardwright@Richards-MacBook-Pro.local>
2020-04-04 15:15:19 +0100

    added pedal model

--- pedal_board_backend/models.py
+++ pedal_board_backend/models.py
@@ -2,9 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+class Pedal(models.Model):
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    image = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
 class Artist(models.Model):
     name = models.CharField(max_length=100)
     band = models.CharField(max_length=100)
     image = models.CharField(max_length=1000)
     wiki = models.CharField(max_length=1000)
+    pedals = models.ManyToManyField(Pedal)
     created_at = models.DateTimeField(auto_now_add=True)
